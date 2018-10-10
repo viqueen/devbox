da6e2e0df13c816a28eb499d8dc5019859c47ea0
viqueen <hasnae@viqueen.org>
2018-10-11 09:58:40 +1100

    enrich with some useful basho functions

--- bin/selfedit.sh
+++ bin/selfedit.sh
@@ -9,6 +9,12 @@ source selfdoc.sh
 
 VIQUEEN_DEVBOX_BIN=${VIQUEEN_DEVBOX_HOME}/bin
 
+# @COMMAND docs [name]                      prints the function definition
+docs() {
+    _with_arguments 1 $@
+    type ${1}
+}
+
 # @COMMAND edit [script]                    opens current or new bin-script
 edit() {
     script=${VIQUEEN_DEVBOX_BIN}/${1}
@@ -30,10 +36,21 @@ _replace() {
     echo ${1//$2/$3}
 }
 
-# @COMMAND join [delim] [elements]          joins elements with delimiter
+# @COMMAND _join [delim] [elements]          joins elements with delimiter
 _join() {
+    _with_arguments 1 $@
     delim=$1
     shift
     output=$(printf "$delim%s" "${@}")
     echo ${output:1}
+}
+
+# @COMMAND _with_arguments [count] [...]     ensures the arguments count is at least as expected
+_with_arguments() {
+    count=${1}
+    shift
+    if [ "$#" -lt ${count} ]; then
+        echo "missing arguments, expected at least ${count} but received $#"
+        exit 1
+    fi
 }
