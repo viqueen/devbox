23554bc2132297db6501552a02a670238cbc3baa
viqueen <hrehioui@atlassian.com>
2019-04-22 18:42:15 +1000

    noissue: scan guava upd8s

--- bin/scan-guava.sh
+++ bin/scan-guava.sh
@@ -6,7 +6,10 @@ then
     exit 1
 fi
 
-mvn -fae -e clean com.atlassian.maven.plugins:banned-packages-maven-plugin:scan \
-    -Dscanned=${1} \
+scanned=${1}
+shift
+
+mvn -fae -e compile com.atlassian.maven.plugins:banned-packages-maven-plugin:1.0.1:scan \
+    -Dscanned=${scanned} \
     -Dbanned="com.google.common.(base|collect).*" \
-    -DfailOnViolation=true
+    $@
