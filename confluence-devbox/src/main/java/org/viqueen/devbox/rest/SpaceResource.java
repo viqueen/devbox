1d014b43951ddf02d672ebbbe0dc1f3a868b1c02
viqueen <hrehioui@atlassian.com>
2019-08-28 11:55:41 +1000

    [noissue] - I need to bulk delete spaces because deving

---
+++ confluence-devbox/src/main/java/org/viqueen/devbox/rest/SpaceResource.java
@@ -0,0 +1,42 @@
+package org.viqueen.devbox.rest;
+
+import com.atlassian.annotations.security.XsrfProtectionExcluded;
+import com.atlassian.confluence.api.model.longtasks.LongTaskSubmission;
+import com.atlassian.confluence.api.model.pagination.SimplePageRequest;
+import com.atlassian.confluence.api.service.content.SpaceService;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Path("/spaces")
+public class SpaceResource {
+
+    private final SpaceService spaceService;
+
+    public SpaceResource(final @ComponentImport SpaceService spaceService) {
+        this.spaceService = spaceService;
+    }
+
+    @DELETE
+    @Produces("application/json")
+    @XsrfProtectionExcluded
+    public Response deleteAll(
+            @QueryParam("start") @DefaultValue("0") final int start,
+            @QueryParam("limit") @DefaultValue("100") final int limit) {
+        final List<LongTaskSubmission> tasks = spaceService.find()
+                .fetchMany(new SimplePageRequest(start, limit))
+                .getResults()
+                .stream()
+                .map(spaceService::delete)
+                .collect(Collectors.toList());
+        return Response.status(Response.Status.ACCEPTED).entity(tasks).build();
+    }
+}
