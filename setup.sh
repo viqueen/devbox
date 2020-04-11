9761947daae5a0cfcbd40b53a190b47672a33868
viqueen <hasnae@viqueen.org>
2020-04-11 18:06:59 +1000

    noissue: extract all that setup box drama into .devboxrc so I dont have to ever touch setup.sh again

--- setup.sh
+++ setup.sh
@@ -3,21 +3,10 @@
 VIQUEEN_DEVBOX_HOME=$(cd "$(dirname "$0")" && pwd -P)
 
 function config_box() {
-    echo "alias ll='ls -la'" >> ~/.bashrc
-    echo "alias please=sudo" >> ~/.bashrc
-
-    echo "export PS1='\u@\h\w'" >> ~/.bashrc
-    echo "export CLICOLOR=1" >> ~/.bashrc
-    echo "export LSCOLORS=GxFxCxDxBxegedabagaced" >> ~/.bashrc
-
     echo "export VIQUEEN_DEVBOX_HOME=${VIQUEEN_DEVBOX_HOME}" >> ~/.bashrc
     echo "export PATH=${PATH}:${VIQUEEN_DEVBOX_HOME}/bin:~/bin" >> ~/.bashrc
-
-    # setup maven
-    echo 'export MAVEN_OPTS="-Xms1680m -Xmx2048m -XX:MaxMetaspaceSize=384m -XX:MaxPermSize=384m"' >> ~/.bashrc
-
-    # setup pretty git log
-    git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+    ln -sfnv ${VIQUEEN_DEVBOX_HOME}/.devboxrc ~/.devboxrc
+    echo "source ~/.devboxrc" >> ~/.bashrc
 }
 
 function config_vim() {
