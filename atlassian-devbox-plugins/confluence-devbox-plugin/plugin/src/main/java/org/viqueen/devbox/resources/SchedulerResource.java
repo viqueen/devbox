6cea2f296099dac9b2d4ea4077501ce70470cfac
viqueen <hasnae@viqueen.org>
2020-08-06 19:47:30 +1000

    noissue: make the resource spit out some more data for inspection

--- atlassian-devbox-plugins/confluence-devbox-plugin/plugin/src/main/java/org/viqueen/devbox/resources/SchedulerResource.java
+++ atlassian-devbox-plugins/confluence-devbox-plugin/plugin/src/main/java/org/viqueen/devbox/resources/SchedulerResource.java
@@ -1,14 +1,20 @@
 package org.viqueen.devbox.resources;
 
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.scheduler.config.JobRunnerKey;
 import com.atlassian.scheduler.core.LifecycleAwareSchedulerService;
+import com.atlassian.scheduler.core.RunningJob;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-import static java.util.Collections.singletonMap;
+import static java.util.stream.Collectors.toSet;
 
 @Path("/scheduler")
 public class SchedulerResource {
@@ -23,7 +29,15 @@ public class SchedulerResource {
     @GET
     @Path("/status")
     public Response status() {
-        return Response.ok(singletonMap("status", serviceController.getState().name())).build();
+        Collection<RunningJob> locallyRunningJobs = serviceController.getLocallyRunningJobs();
+        Set<JobRunnerKey> registeredJobs = serviceController.getRegisteredJobRunnerKeys();
+        Set<JobRunnerKey> scheduledJobs = serviceController.getJobRunnerKeysForAllScheduledJobs();
+        Map<String, Object> data = new HashMap<>();
+        data.put("status", serviceController.getState().name());
+        data.put("running", locallyRunningJobs);
+        data.put("registered", registeredJobs.stream().map(JobRunnerKey::toString).collect(toSet()));
+        data.put("scheduled", scheduledJobs.stream().map(JobRunnerKey::toString).collect(toSet()));
+        return Response.ok(data).build();
     }
 
 }
