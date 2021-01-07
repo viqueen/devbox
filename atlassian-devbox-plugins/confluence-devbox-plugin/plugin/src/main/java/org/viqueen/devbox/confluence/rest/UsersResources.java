d2598439510fb92d0bf1b64ab5b72d5379e198bc
viqueen <hrehioui@atlassian.com>
2021-01-07 16:47:35 +1100

    noissue: welcome user resource to get user initials so I can generate some avatars

---
+++ atlassian-devbox-plugins/confluence-devbox-plugin/plugin/src/main/java/org/viqueen/devbox/confluence/rest/UsersResources.java
@@ -0,0 +1,51 @@
+package org.viqueen.devbox.confluence.rest;
+
+import com.atlassian.confluence.user.UserAccessor;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.user.Entity;
+import com.atlassian.user.User;
+import com.atlassian.user.search.page.Pager;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+import static java.util.stream.Collectors.toMap;
+
+@Path("/users")
+public class UsersResources {
+
+    private final UserAccessor userAccessor;
+
+    @Autowired
+    public UsersResources(
+            @ComponentImport final UserAccessor userAccessor
+    ) {
+        this.userAccessor = userAccessor;
+    }
+
+    @GET
+    @Path("/initials")
+    public Response getInitials() {
+        final Pager<User> users = userAccessor.getUsers();
+        final Map<String, String> initials = users.getCurrentPage()
+                .stream()
+                .collect(toMap(
+                        Entity::getName,
+                        u -> {
+                            String fullName = u.getFullName();
+                            String[] userInitials = stream(fullName.split(" "))
+                                    .map(name -> String.valueOf(name.charAt(0)))
+                                    .toArray(String[]::new);
+                            return String.join("", userInitials);
+                        }
+                ));
+        return Response.ok(initials).build();
+    }
+
+}
