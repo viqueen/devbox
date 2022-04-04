43f887dec6f71522a7088173519bd35ea48e44e2
Hasnae <hasnae@viqueen.org>
2022-04-04 14:53:58 +1000

    scan docker images

--- cli/bin/selfedit.sh
+++ cli/bin/selfedit.sh
@@ -9,10 +9,20 @@ source selfdoc.sh
 
 VIQUEEN_DEVBOX_BIN=${VIQUEEN_DEVBOX_HOME}/cli/bin
 
+# COLORS
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+WHITE='\033[0;37m'
+COLOR_OFF='\033[0m'
+
 # @COMMAND docs [name]                          prints the function definition
 docs() {
-    _with_arguments 1 $@
-    type ${1}
+    _with_arguments 1 "$@"
+    type "${1}"
 }
 
 # @COMMAND edit [script]                        opens current or new bin-script
@@ -22,27 +32,27 @@ edit() {
     then
         script=${0}
     fi
-    vim ${script}
-    chmod +x ${script}
+    vim "${script}"
+    chmod +x "${script}"
 }
 
 # @COMMAND _default                             returns first defined argument
 _default() {
-    echo $1
+    echo "$1"
 }
 
 # @COMMAND _replace [str] [char] [repl]         replace occurrence in string
 _replace() {
-    echo ${1//$2/$3}
+    echo "${1//$2/$3}"
 }
 
 # @COMMAND _join [delim] [elements]             joins elements with delimiter
 _join() {
-    _with_arguments 1 $@
+    _with_arguments 1 "$@"
     delim=$1
     shift
     output=$(printf "$delim%s" "${@}")
-    echo ${output:1}
+    echo "${output:1}"
 }
 
 # @COMMAND _with_arguments [count] [...]        ensures the arguments count is at least as expected
@@ -57,16 +67,17 @@ _with_arguments() {
 
 # @COMMAND _with_functions [names...]           ensures the functions are available on the parent script
 _with_functions() {
-    _with_arguments 1 $@
+    _with_arguments 1 "$@"
     errors=()
     for item in "$@"
     do
-        if [[ `type -t ${item}`"" != 'function' ]]; then
-            errors+=(${item})
+        if [[ $(type -t "${item}")"" != 'function' ]]; then
+            errors+=("${item}")
         fi
     done
 
     if [[ ${#errors[@]} -gt 0 ]]; then
+        # shellcheck disable=SC2145
         echo "missing impl for function(s) : ${errors[@]}"
         exit 1
     fi
--- docker-images/maven-java/jdk.Dockerfile
+++ docker-images/maven-java/jdk.Dockerfile
@@ -1,4 +1,7 @@
 FROM maven:${JDK_VERSION}
 
 RUN apt-get update \
-    && apt-get install -y git nodejs npm wget
+    && apt-get install -y git wget \
+    && wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash \
+    && . ~/.bashrc \
+    && nvm install 17.8.0
