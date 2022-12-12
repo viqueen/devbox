6f89fe50a65d573d4b00c45f6ae48e1e4b3bcd6e
Hasnae <hasnae@viqueen.org>
2022-12-12 15:50:05 +1100

    migrate hub script to use axios cli, and copy pasta few handlers from old cli-rest-client because monday

--- cli/lib/axios-cli.js
+++ cli/lib/axios-cli.js
@@ -2,6 +2,37 @@
 
 const Command = require("commander").Command;
 const axios = require("axios");
+const queryString = require("query-string");
+
+const paramRegex = /(?<key>[a-zA-Z]+)=(?<value>.*)/;
+
+function collect(val, memo) {
+  memo.push(val);
+  return memo;
+}
+
+function extractParameters(parameters) {
+  const query = {};
+  parameters.forEach((parameter) => {
+    const matcher = parameter.match(paramRegex);
+    if (matcher) {
+      const key = matcher.groups["key"];
+      const value = matcher.groups["value"];
+      if (value === "true") {
+        query[key] = true;
+      } else if (value === "false") {
+        query[key] = false;
+      } else {
+        query[key] = encodeURI(value);
+      }
+    }
+  });
+  return query;
+}
+
+function makeDataObject(fromBase, fromOptions) {
+  return Object.assign({}, fromBase, extractParameters(fromOptions));
+}
 
 class AxiosCli {
   constructor(options) {
@@ -19,13 +50,15 @@ class AxiosCli {
       commander
         .command(`${method} [parts...]`)
         .description(`${method} ${this.name} resources`)
-        .action(async (parts) => {
+        .option("-q, --query [value]", "set the request query", collect, [])
+        .action(async (parts, options) => {
+          const query = makeDataObject({}, options.query);
           const token = await this.auth.bearerToken();
           const headers = token ? { Authorization: `Bearer ${token}` } : {};
           this.client
             .request({
               method: method,
-              url: `/${parts.join("/")}`,
+              url: `/${parts.join("/")}?${queryString.stringify(query)}`,
               headers,
             })
             .then(({ data }) => console.log(JSON.stringify(data)))
