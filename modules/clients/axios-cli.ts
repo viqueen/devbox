688417c332d50d9d3c01af74307661fbe0e5ebf3
viqueen <hasnae@viqueen.org>
2023-05-09 10:17:21 +1000

    axios cli client to process query params too

--- modules/clients/axios-cli.ts
+++ modules/clients/axios-cli.ts
@@ -1,10 +1,43 @@
 import { Command } from 'commander';
 import axios, { AxiosInstance } from 'axios';
+import * as queryString from 'query-string';
 
 interface AxiosCliProps {
     name: string;
     baseURL: string;
 }
+
+const collect = (value: string, data: string[]) => {
+    data.push(value);
+    return data;
+};
+
+const paramRegex = /(?<key>[a-zA-Z]+)=(?<value>.*)/;
+
+const extractParameters = (parameters: string[]) => {
+    const query: Record<string, string | boolean> = {};
+    parameters.forEach((parameter) => {
+        const matcher = parameter.match(paramRegex);
+        if (matcher === null || matcher.groups === undefined) return;
+        const key = matcher.groups['key'];
+        const value = matcher.groups['value'];
+        if (value === 'true') {
+            query[key] = true;
+        } else if (value === 'false') {
+            query[key] = false;
+        } else {
+            query[key] = encodeURI(value);
+        }
+    });
+    return query;
+};
+
+const makeDataObject = (
+    fromBase: Record<string, string | boolean>,
+    fromOptions: string[]
+) => {
+    return Object.assign({}, fromBase, extractParameters(fromOptions));
+};
 
 class AxiosCli {
     private readonly client: AxiosInstance;
@@ -20,11 +53,20 @@ class AxiosCli {
             _program
                 .command(`${method} [parts...]`)
                 .description(`${method} ${this.props.name} resources`)
-                .action(async (parts) => {
+                .option(
+                    '-q, --query [value]',
+                    'set the request query',
+                    collect,
+                    []
+                )
+                .action(async (parts, options) => {
+                    const query = makeDataObject({}, options.query);
                     this.client
                         .request({
                             method,
-                            url: `${parts.join('/')}`
+                            url: `${parts.join('/')}?${queryString.stringify(
+                                query
+                            )}`
                         })
                         .then(({ data }) => console.info(JSON.stringify(data)))
                         .catch((error) => {
