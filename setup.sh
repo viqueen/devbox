2020a97b78bb55a10a70205b4ad3f868ef83df64
viqueen <hasnae@viqueen.org>
2023-06-20 20:10:09 +1000

    detect shell rc in setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,22 @@
 
 VIQUEEN_DEVBOX_HOME=$(cd "$(dirname "$0")" && pwd -P)
 
+_shell_rc() {
+  if [[ -n $ZSH_VERSION ]]; then
+    echo "${HOME}/.zshrc"
+  elif [[ -n $FSH_VERSION ]]; then
+    echo "${HOME}/.fshrc"
+  else # default to bash
+    echo "${HOME}/.bashrc"
+  fi
+}
+
 function config_box() {
-    echo "export VIQUEEN_DEVBOX_HOME=${VIQUEEN_DEVBOX_HOME}" >> ~/.bashrc
-    echo "export PATH=${PATH}:${VIQUEEN_DEVBOX_HOME}/modules/cli/bin:${VIQUEEN_DEVBOX_HOME}/cli/bin:~/bin" >> ~/.bashrc
+    local -r rc_file=$(_shell_rc)
+    echo "export VIQUEEN_DEVBOX_HOME=${VIQUEEN_DEVBOX_HOME}" >> "${rc_file}"
+    echo "export PATH=${PATH}:${VIQUEEN_DEVBOX_HOME}/modules/cli/bin:${VIQUEEN_DEVBOX_HOME}/cli/bin:~/bin" >> "${rc_file}"
     ln -sfnv "${VIQUEEN_DEVBOX_HOME}"/cli/.devboxrc ~/.devboxrc
-    echo "source ~/.devboxrc" >> ~/.bashrc
+    echo "source ~/.devboxrc" >> "${rc_file}"
 }
 
 function config_vim() {
@@ -18,8 +29,9 @@ function config_vim() {
 }
 
 function config_prompt() {
+    local -r rc_file=$(_shell_rc)
     ln -sfnv "${VIQUEEN_DEVBOX_HOME}"/cli/.promptline.sh ~/.promptline.sh
-    echo "source ~/.promptline.sh" >> ~/.bashrc
+    echo "source ~/.promptline.sh" >> "${rc_file}"
 }
 
 # shellcheck disable=SC2068
