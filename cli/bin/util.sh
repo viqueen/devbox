d5da67e9b6b5810df0dd1e837b482ea05a72d941
viqueen <hasnae.rehioui@safetyculture.io>
2023-08-07 13:18:11 +1000

    welcome util.sh at last

---
+++ cli/bin/util.sh
@@ -0,0 +1,18 @@
+# @COMMAND _join [delim] [elements]             joins elements with delimiter
+_join() {
+    _with_arguments 1 "$@"
+    delim=$1
+    shift
+    output=$(printf "$delim%s" "${@}")
+    echo "${output:1}"
+}
+
+# @COMMAND _with_arguments [count] [...]        ensures the arguments count is at least as expected
+_with_arguments() {
+    count=${1}
+    shift
+    if [[ "$#" -lt ${count} ]]; then
+        echo "missing arguments, expected at least ${count} but received $#"
+        exit 1
+    fi
+}
