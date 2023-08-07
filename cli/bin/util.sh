43d88907a1ef825ca3a433a8901dde11770567c7
viqueen <hasnae.rehioui@safetyculture.io>
2023-08-07 14:57:47 +1000

    omg fix that join function, bad copy pasta

--- cli/bin/util.sh
+++ cli/bin/util.sh
@@ -1,12 +1,3 @@
-# @COMMAND _join [delim] [elements]             joins elements with delimiter
-_join() {
-    _with_arguments 1 "$@"
-    delim=$1
-    shift
-    output=$(printf "$delim%s" "${@}")
-    echo "${output:1}"
-}
-
 # @COMMAND _with_arguments [count] [...]        ensures the arguments count is at least as expected
 _with_arguments() {
     count=${1}
@@ -15,4 +6,13 @@ _with_arguments() {
         echo "missing arguments, expected at least ${count} but received $#"
         exit 1
     fi
+}
+
+# @COMMAND _join [delim] [elements]             joins elements with delimiter
+_join() {
+    _with_arguments 1 "$@"
+    delim=$1
+    shift
+    output=$(printf -- "$delim%s" "${@}")
+    echo "${output:1}"
 }
