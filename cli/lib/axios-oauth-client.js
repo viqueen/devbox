4a75422a90939f6ebb933424f2101a7a61ba6d65
viqueen <hasnae.rehioui@safetyculture.io>
2025-06-20 11:47:32 +1000

    extract myspotify command out of typescript land

--- cli/lib/axios-oauth-client.js
+++ cli/lib/axios-oauth-client.js
@@ -1,12 +1,29 @@
-const axios = require('axios');
+/**
+ * Copyright 2025 Hasnae Rehioui
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const crypto = require('crypto');
-const express = require('express');
 const fs = require('fs');
 const http = require('http');
-const open = require('open');
 const os = require('os');
 const path = require('path');
 const qs = require('querystring');
+
+const axios = require('axios');
+const express = require('express');
+const open = require('open');
 
 class AxiosOauthClient {
     constructor(options) {
@@ -101,7 +118,7 @@ class AxiosOauthClient {
             return;
         }
         const payload = JSON.parse(fs.readFileSync(target).toString());
-        // TODO: handle refresh token, also I need typescript
+        // TODO: handle refresh token
         // noinspection JSUnresolvedVariable
         return payload.access_token;
     }
